e10536f93c922ece86c32061f67bcb3b70886baa
Danchiiik <dcabatar@gmail.com>
2023-07-03 18:25:24 +0600

    driver_id, hide codes, date_format

--- applications/job/serializers.py
+++ applications/job/serializers.py
@@ -11,6 +11,15 @@ class JobSerializer(serializers.ModelSerializer):
         model = Job
         fields = '__all__'
         
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        exclude_fields = ['activation_code', 'complete_code', 'cancel_code']
+        for field in exclude_fields:
+            rep.pop(field, None)
+        return rep
+        
+        
 
 class JobOfferSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
@@ -21,13 +30,17 @@ class JobOfferSerializer(serializers.ModelSerializer):
         
     def update(self, instance, validated_data):
         if instance.status == 'Delivering':
-            raise serializers.ValidationError('Sorry, but this job is taken by someone!')
+            raise serializers.ValidationError('Sorry, but this job was taken by someone!')
 
         email_shipper = instance.owner.email
         email_carrier = self.context['request'].user.email
-        # send_email_to_owner.delay(email_shipper, email_carrier)
+        carrier_id = self.context['request'].user.id
+        send_email_to_owner.delay(email_shipper, email_carrier)
         send_confirmation_email.delay(email_carrier, instance.activation_code, instance.cancel_code, instance.id)
-        instance.status = 'Negotiations are underway'
+        
+        instance.status = 'Loading goods'
+        instance.driver_id = carrier_id
         instance.save()
     
         return instance
+
--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
+
+DATE_FORMAT = 'd-m-Y'
 
 USE_I18N = True
 
