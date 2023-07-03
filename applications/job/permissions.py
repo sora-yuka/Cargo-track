8a692438f1c65bdb807b7800fbe43d80f03980ec
Danchiiik <dcabatar@gmail.com>
2023-07-03 14:12:42 +0600

    permissions

--- applications/job/permissions.py
+++ applications/job/permissions.py
@@ -1,14 +1,20 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
+from applications.job.models import Job
 
-class ShipperOnly(BasePermission):
+
+class IsShipper(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS or request.method == 'POST':
+            return True
+        return request.user.is_authenticated and request.user == Job.objects.get(id=view.kwargs['pk']).owner
+    
+    
+class IsCompanyOrCarrier(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
-        if request.method == 'POST':
-            return True
+        # return request.user.is_authenticated and request.user. != Job.objects.get(id=view.kwargs['pk']).owner.role
+    
         
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user.is_authenticated and (request.user == obj.owner)
+        
